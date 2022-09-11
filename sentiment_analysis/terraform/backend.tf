c3da5d0e21712961864bd7bbb132836f02190e4e
Nickolas Rodriguez <nickotronz7@gmail.com>
2022-09-11 12:29:36 -0600

    Revert "Revert "gcs backend""
    This reverts commit 259d2e5da3e7b576e7484a9539f97ca2eeafac79.

--- sentiment_analysis/terraform/backend.tf
+++ sentiment_analysis/terraform/backend.tf
@@ -3,12 +3,8 @@
 # Using Terraform Cloud Workspace to store the terraform state and share it with the team
 # The workspace excecution is set up local, thus, Terraform Cloud just store the terraform states
 terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "soa-orga-spot"
-
-    workspaces {
-      name = "soa-orga-workspace"
-    }
+  backend "gcs" {
+    bucket = "tfstate-bucket-soa"
+    prefix = "/terraform.tfstate"
   }
 }
