d69d2a28825c988baf53ba09837db75db935d7c8
Nickolas Rodriguez <nickotronz7@gmail.com>
2022-09-15 21:05:05 -0600

    file name updated

--- sentiment_analysis/src/unittest.py
+++ sentiment_analysis/src/unittest.py
@@ -10,6 +10,6 @@ def test_analyze_emotion():
     Unit test
     """
     result = main.analyze_emotion(
-        {"name": "unit-test.jpg", "bucket": "tfstate-bucket-soa"}, 0)
+        {"name": "unittest.jpg", "bucket": "tfstate-bucket-soa"}, 0)
 
     assert result == "Happy"
